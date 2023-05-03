20fccaf189a070110bd73a1f45587292967e776e
soumya210611 <soumya210611@gmail.com>
2023-05-03 18:57:42 +0530

    upload code

---
+++ mavenjavaproject/src/main/java/mavenjavaprojectdemo/demoJava.java
@@ -0,0 +1,15 @@
+package mavenjavaprojectdemo;
+
+public class demoJava {
+	public void display() {
+		System.out.println("hello");
+	}
+public void show() {
+	System.out.println("nature");
+}
+public static void main(String args[]) {
+	demoJava demo= new demoJava();
+	demo.display();
+	demo.show();
+}
+}
